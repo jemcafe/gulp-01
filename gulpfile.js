e65cb222330eeae393154800c9e4b05d75875d8d
Jemaine Brown <jembrown@Jemaines-MacBook-Pro.local>
2018-08-05 18:31:30 -0500

    gulp optimizes image

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 // The file must be called gulpfile
 const gulp = require('gulp');
+const imagemin = require('gulp-imagemin');  // optimizes image files
 
-/*
+/*  TOP LEVEL FUNCTIONS
   gulp.task - define tasks
   gulp.src - point to file
   gulp.dest - output destination
@@ -10,6 +11,17 @@ const gulp = require('gulp');
 
 gulp.task('message', function(){
   return console.log('Gulp is running');
+});
+
+gulp.task('copyHtml', function(){
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
+});
+
+gulp.task('imageMin', function(){
+  gulp.src('src/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('dist/images'));
 });
 
 gulp.task('default', function(){
